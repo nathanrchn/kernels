bec72e9feb5b7071909e139c961cedf02e7729dd
Nathan Ranchin <nranchin@student.ethz.ch>
2025-11-05 15:59:40 +0100

    troch impl

--- xielu.py
+++ xielu.py
@@ -1,2 +1,8 @@
+import torch
 import tilelang
 import tilelang.language as T
+
+def torch_xielu(x: torch.Tensor, a_p: float = 0.8, a_n: float = 0.8, beta: float = 0.5, eps: float = 1e-6) -> torch.Tensor:
+    alpha_p = torch.nn.functional.softplus(a_p)
+    alpha_n = beta + torch.nn.functional.softplus(a_n)
+    return torch.where(x > 0, alpha_p * x * x + beta * x, alpha_n * torch.expm1(torch.clamp_max(x, eps)) - alpha_n * x + beta * x)
