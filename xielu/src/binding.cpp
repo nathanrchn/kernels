91f3748456c76c92f522b832587dd4fdb53b97e5
Nathan Ranchin <nranchin@student.ethz.ch>
2025-12-06 23:16:01 -0800

    torch.compile fix fix

--- xielu/src/binding.cpp
+++ xielu/src/binding.cpp
@@ -10,3 +10,8 @@ TORCH_LIBRARY_IMPL(xielu, CUDA, m) {
     m.impl("forward", &xielu_forward);
     m.impl("backward", &xielu_backward);
 }
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("forward", &xielu_forward);
+    m.def("backward", &xielu_backward);
+}
